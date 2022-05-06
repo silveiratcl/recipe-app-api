f958f59b96cb3f4d6f8720394c4d8804f740ac75
Thiago Silveira <62719017+silveiratcl@users.noreply.github.com>
2022-05-06 20:40:44 -0300

    fixes in tests admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,21 +13,16 @@ class UserAdmin(BaseUserAdmin):
         (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
-            {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
-                )
-            }
+            { 'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields': ('last_login',)}),
-)
-add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'password1', 'password2')
-    }),
-)
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin) 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,17 +8,17 @@ class AdminSiteTests(TestCase):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@london.com',
-            password ='password123'
+            password='password123'
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.create(
+        self.user = get_user_model().objects.create_user(
             email='test@london.com',
             password='password123',
-            name='Test user full name'
+            name='Test User Full Name'
         )
 
-    def test_user_listed(self):
-        """Test that users are listed on user page"""
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
